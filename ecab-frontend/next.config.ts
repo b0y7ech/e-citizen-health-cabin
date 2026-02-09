b64de0c95b9d6bca031c6ec0be990d8df2072a38
GoTRUST-Tung <tung.nguyen@gotrust.vn>
2026-02-10 00:26:01 +0700

    - add docker file

--- ecab-frontend/next.config.ts
+++ ecab-frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "standalone", // For Docker build
 };
 
 export default nextConfig;
